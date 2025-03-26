a9dcbf4fcfd319f9cafbd3da7e1f9580f3fcb61f
dungpham116 <dung.pham@jesselton.capital>
2025-03-26 15:52:18 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,15 @@ const nextConfig = {
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/index/:path*',
+        destination: '/api/index/:path*',
+      },
+    ]
+  }
+  
 }
 
 mergeConfig(nextConfig, userConfig)
