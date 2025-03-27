65e8112a214926251d035e01c32b03486d1f3f5d
dungpham116 <dung.pham@jesselton.capital>
2025-03-27 18:06:21 +0700

    Update site-header.tsx

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -8,7 +8,7 @@ export function SiteHeader() {
     <header className="sticky top-0 z-50 w-full border-b bg-background shadow-sm">
       <div className="container flex h-16 items-center">
         <Link href="/" className="font-bold text-xl mr-8 ml-6">
-          Funding Platform
+          HR Monster
         </Link>
         <MainNav className="mx-8" />
         <div className="ml-auto flex items-center space-x-6">
